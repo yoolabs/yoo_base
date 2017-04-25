e0c0284c99613fd6a61c0e5bcd30c935aba78044
jbjhjm <j_mewes@web.de>
2017-04-25 11:54:59 +0200

    misc: add improved html layouts for mod_search, com_search, mod_finder, com_finder

---
+++ template/html/com_search/search/default.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_search
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+JHtml::_('formbehavior.chosen', 'select');
+?>
+
+<div class="search<?php echo $this->pageclass_sfx; ?>">
+
+<h1 class="page-title">
+	Suchergebnisse
+</h1>
+
+
+<?php #echo $this->loadTemplate('form'); ?>
+<?php if ($this->error == null && count($this->results) > 0) :
+	echo $this->loadTemplate('results');
+else :
+	echo $this->loadTemplate('error');
+endif; ?>
+</div>
