ba6600018fe60fc7a5539ce8c04dd88a5cae9f24
jbjhjm <j_mewes@web.de>
2018-04-30 16:50:36 +0200

    template.js Update to new wi.snapscroll setup

--- development/template/js/template.js
+++ development/template/js/template.js
@@ -2,43 +2,23 @@
 
 
 /*
-	custom code...
+custom code...
 */
 
 (function($){
 	$(document).ready(function(){
 
-		var config = $('html').data('config') || {};
-		var isMediumDevice = window.matchMedia('(min-width:768px)').matches;
-		var isLargeDevice = window.matchMedia('(min-width:960px)').matches;
-
+		// var config = $('html').data('config') || {};
+		// var isLargeDevice = window.matchMedia('(min-width:960px)');
+		// var $body = $('body');
 
 		/* ----------- SNAP SCROLL ------------------------------ */
-
-		if(isLargeDevice) {
-
-			$('html').addClass('snapscroll');
-			var $page = $(".page-layout");
-			var $onepage = $(".onepage-wrapper");
-			// var frameWidth = parseInt($page.css('top'));
-
-			$onepage.onepage_scroll({
+		var $onepage = $(".onepage-wrapper");
+		if($onepage.length) {
+			$onepage.wiSnapScroll({
 				sectionContainer: ".onepageblock",
 				paginationContainer: "#dotnav-inner",
-				// globalScrollOffset: frameWidth,
-				// responsiveFallback: 768, // does not work.
 				direction: "vertical",
-				beforeMove: function(index) {
-					setTimeout(function() {
-						var $target = $onepage.children('.onepageblock[data-index="'+index+'"]');
-						var $els = $target.find('[data-uk-scrollspy]');
-						console.log($target,$els);
-						$els.addClass('uk-scrollspy-init-inview').trigger('inview.uk.scrollspy');
-					},500);
-				},
-				afterMove: function(index) {
-
-				},
 			});
 		}
 
